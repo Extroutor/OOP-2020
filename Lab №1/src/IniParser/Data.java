0b2adae53e6e915013f3bedc34a37ab845b6a52c
Extroutor <58516932+Extroutor@users.noreply.github.com>
2020-10-04 17:33:05 +0300

    Update Data.java

--- Lab №1/src/IniParser/Data.java
+++ Lab №1/src/IniParser/Data.java
@@ -1,41 +1,53 @@
 package IniParser;
 
 import Exceptions.PropOrSecNotFoundException;
+import Exceptions.TypeMismatchException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Vector;
 
 public class Data {
-    private HashMap<String, Vector<String>> map;
-    Getters get = new Getters();
-    String _type;
+    private final HashMap<String, ArrayList<String>> map;
 
-    public Data(HashMap _map) {
+    public Data(HashMap<String, ArrayList<String>> _map) {
         map = _map;
     }
 
+    public int getInt (String section, String parameter) throws RuntimeException {
 
-    public String p(String section, String parameter) throws RuntimeException {
+        String string = result(section, parameter);
+        try {
+            return Integer.parseInt(string);
+        } catch (Exception e) {
+            throw new TypeMismatchException("Error: cannot parse to int");
+        }
+
+    }
+
+    public String getString (String section, String parameter) throws RuntimeException {
+
+            return result(section, parameter);
+    }
+
+    public float getFloat(String section, String parameter) throws RuntimeException {
+
+        String string = result(section, parameter);
+        try {
+            return Float.parseFloat(string);
+        } catch (Exception e) {
+            throw new TypeMismatchException("Error: cannot parse to float");
+        }
+
+    }
+
+    public String result (String section, String parameter) throws RuntimeException {
+
         try {
             int n = map.get(section).indexOf(parameter);
             return map.get(section).get(n + 1);
         } catch (Exception e) {
             throw new PropOrSecNotFoundException("Error: not such section or property");
         }
-    }
 
-    public void pArsing(String section, String parameter) throws RuntimeException {
-        String value = p(section, parameter);
-        if (_type.equals("Integer"))
-            System.out.println(get.getInt(value));
-        else if (_type.equals("Float"))
-            System.out.println(get.getFloat(value));
-        else
-            System.out.println(value);
-    }
-
-    public void getValue(String section, String parameter, String type) throws RuntimeException {
-        _type = type;
-        pArsing(section, parameter);
     }
 }
